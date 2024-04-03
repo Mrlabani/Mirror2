7e2b39e88f765a76dfded02ee1a7bf9b76810234
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-04-04 00:19:53 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -1,5 +1,5 @@
 apt install git -y
-git clone https://github.com/https-github-com-rooted-cyber/helios-mirror
-cd helios*
+git clone https://github.com/Mrlabani/mirror1
+cd mirror1*
 pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
