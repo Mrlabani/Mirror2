f8ff3ea21eb04b84c28ca4195931ba71bd110129
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-04-04 07:28:06 +0530

    Update maruf.sh

--- maruf.sh
+++ maruf.sh
@@ -1,5 +1,5 @@
 apt install git -y
-git https://github.com/mrnoobx/WZML-X
-cd WZML-X
+git clone https://github.com/mrnoobx/WZML-X.git
+cd WZML-X*
 pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
